2b8449c702505da747f93a163ba3a16e3ee8139a
Augusto Etz <augustoetz@hotmail.com>
2022-06-21 19:07:59 -0300

    exericicos bloco 3

---
+++ FUNDAMENTOS/bloco-04-Introdução-JS/scripts.js
@@ -0,0 +1,15 @@
+const myName = "Augusto Etz";
+const birthCity = "Barbacena";
+let birthYear = 1998;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030
+
+console.log(birthYear);
+
+birthCity = "alabamas";
+
+console.log(birthCity);
